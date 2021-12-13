c565745511230a1104014cf7762e5a4379e3ecb0
Kirill Paraschenko <kparaschenko@caeonline.com>
2021-12-13 20:24:08 +0300

    Task  20 Added Creation and Removing project tests in Mantis

---
+++ mantis-web-tests/tests/ProjectCreationTests.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System.Linq;
+
+namespace mantis_web_tests
+{
+    [TestFixture]
+    public class ProjectCreationTests : AuthTestBase
+    {
+
+        [Test]
+        public void ProjectCreationTest()
+        {
+            ProjectData project = new ProjectData("Test project");
+
+            List<ProjectData> oldProjects = app.Projects.GetProjectsList();
+
+            app.Projects.Create(project);
+
+            List<ProjectData> newProjects = app.Projects.GetProjectsList();
+            oldProjects.Add(project);
+            oldProjects.Sort();
+            newProjects.Sort();
+            Assert.AreEqual(oldProjects, newProjects);
+        }
+    }
+}
